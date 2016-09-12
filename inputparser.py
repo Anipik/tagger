dcdb9633562e08a6f6f3a07d98d612c7963882ad
Anipik <anirudhagnihotry098@gmail.com>
2016-09-12 22:14:01 +0530

    word per line

--- inputparser.py
+++ inputparser.py
@@ -1,4 +1,5 @@
 import nltk.data
+from nltk.tokenize import wordpunct_tokeniz
 
 
 def conv_sentperline(filename):
@@ -7,10 +8,35 @@ def conv_sentperline(filename):
 	string = fo.read()
 	string =' '.join(string.split())
 	sent = sent_detector.tokenize(string.strip())
-	sent = '\n'.join(sent)
+	sent_join = '\n'.join(sent)
 	fo.seek(0)
-	fo.write(sent)
+	fo.write(sent_join)
 	fo.close
+	return sent
+
+
+
+def file_to_datapoint(filename):
+	string=""
+	tag = "O"
+	sentences = conv_sentperline(filename)
+	for sent in sentences:
+		words = wordpunct_tokenize(sent)
+		for word in words:
+			string = string + word +'\t' + tag+'\n'
+		string = string + '\n'
+
+	return string[:-1]
+
+
+        
+
+
+
+
+
+
+
 
 
 conv_sentperline("big.txt")
